8cc233edb2813f3bd0467ee2dff1d4cf2703d356
Sylvia Mumbi <mumbi099@gmail.com>
2023-06-26 04:50:06 +0300

    Added the operation methods for +,-,*,/

--- calc-app.rb
+++ calc-app.rb
@@ -9,3 +9,25 @@ operation = gets.chomp
 print "Enter the second number: "
 number2 = gets.chomp.to_f
 
+def addition(num1, num2)
+    return num1 + num2
+end
+result = addition(number1, number2)
+
+def subtraction(num1, num2)
+    return num1 - num2
+end
+result = subtraction(number1, number2)
+  
+def division(num1, num2)
+    return num1 / num2
+end
+result = division(number1, number2)
+  
+def multiplication(num1, num2)
+    return num1 * num2
+end
+result = multiplication(number1, number2)
+  
+
+  
