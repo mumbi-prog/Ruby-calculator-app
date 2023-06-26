765da8b56a05281a09e46c93d0ef555c81dbe018
Sylvia Mumbi <mumbi099@gmail.com>
2023-06-26 04:37:26 +0300

    Enter user input

---
+++ calc-app.rb
@@ -0,0 +1,11 @@
+puts "SIMPLE CALCULATOR"
+
+print "Enter the first number: "
+number1 = gets.chomp.to_f
+
+print "Operator: "
+operation = gets.chomp
+
+print "Enter the second number: "
+number2 = gets.chomp.to_f
+
